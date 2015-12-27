91382513292996e6acf8db4679582f0a1693d037
Javier <jduarte@caltech.edu>
2015-12-26 20:08:10 -0800

    fixed setup.sh

--- BSMGen/setup.sh
+++ BSMGen/setup.sh
@@ -1,3 +1,4 @@
 export PYTHIA8DATA=../pythia/share/Pythia8/xmldoc/
 export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:../hepmc/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../hepmc/lib/
 #source ../pythia/examples/config.sh
