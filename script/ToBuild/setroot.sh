a6f1ff1cf95d43e286114810201c3ae0d5477825
Thong <thongnguyen1292@gmail.com>
2017-02-26 23:31:34 +0100

    add pythia cards for Thong

--- script/ToBuild/setroot.sh
+++ script/ToBuild/setroot.sh
@@ -1,4 +1,4 @@
-export ROOTSYS=/Users/gautam/root
+export ROOTSYS=~/Downloads/buildroot/myroot/
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$PYTHONDIR/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH
 export PATH=${PATH}:${ROOTSYS}/bin
